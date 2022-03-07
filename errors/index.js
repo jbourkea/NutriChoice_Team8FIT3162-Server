e0771c73f0fb67e667067b6452a481d58bf88e1f
Joshua Tait Bourke <jbou0010@student.monash.edu>
2022-03-07 18:18:27 +1100

    Added index.js to errors directory for ease of use

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+import BadRequestError from './BadRequestError.js'
+import ConflictError from './ConflictError.js'
+import NotFoundError from './NotFoundError.js'
+
+export {BadRequestError, ConflictError, NotFoundError}
