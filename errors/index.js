85826cb2a7ef6c48369aafadbba9a1ad6275e54f
Joshua Tait Bourke <jbou0010@student.monash.edu>
2022-03-24 17:49:18 +1100

    Added ForbiddenError

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,6 @@
 import BadRequestError from './BadRequestError.js'
 import ConflictError from './ConflictError.js'
 import NotFoundError from './NotFoundError.js'
+import ForbiddenError from './ForbiddenError.js'
 
-export {BadRequestError, ConflictError, NotFoundError}
+export {BadRequestError, ConflictError, NotFoundError, ForbiddenError}
