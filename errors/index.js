12555ba8e3a5d708cd5eb143ac0f3c5d127ff7d4
Joshua Tait Bourke <jbou0010@student.monash.edu>
2022-03-30 18:14:25 +1100

    Added Unauthorised Error and updated error index

--- errors/index.js
+++ errors/index.js
@@ -2,5 +2,6 @@ import BadRequestError from './BadRequestError.js'
 import ConflictError from './ConflictError.js'
 import NotFoundError from './NotFoundError.js'
 import ForbiddenError from './ForbiddenError.js'
+import UnauthorizedError from './UnauthorizedError.js'
 
-export {BadRequestError, ConflictError, NotFoundError, ForbiddenError}
+export {BadRequestError, ConflictError, NotFoundError, ForbiddenError, UnauthorizedError}
